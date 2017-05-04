dd29ff3b36231c73a6dc8f3c8cc50e3036706e0e
nowyTR <dominik.nowik@synerise.com>
2017-05-04 05:21:37 +0200

    added restart feature

--- js/app.js
+++ js/app.js
@@ -1,43 +1,46 @@
 import fetch from 'isomorphic-fetch'
 
-const actionButton = document.querySelector('.quiz-btn')
-const questionsField = document.querySelector('.quiz-questions')
-let countdown
-const quizTimer = document.querySelector('.quiz-timer')
+function app () {
+  const actionButton = document.querySelector('.quiz-btn')
+  const questionsField = document.querySelector('.quiz-questions')
+  let countdown
+  const quizTimer = document.querySelector('.quiz-timer')
+  const quizResults = document.querySelector('.quiz-results')
 
-actionButton.onclick = () => startQuiz()
+  actionButton.onclick = () => startQuiz()
 
-const Quiz = {
-  timer: 0,
-  questions: [],
-  userPoints: 0,
-  currentQuestion: 0,
-  correctAnswer: 0
-}
-
-function getData () {
-  const URL = 'https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json'
-
-  function processData (data) {
-    Quiz.timer = data.time_seconds
-    Quiz.questions = data.questions
-    console.log(Quiz)
+  const Quiz = {
+    timer: 0,
+    questions: [],
+    userPoints: 0,
+    currentQuestion: 0,
+    correctAnswer: 0
   }
 
-  return fetch(URL)
-    .then(response => response.json())
-    .then(data => processData(data))
-    .catch(error => console.log('BAD', error))
-}
-getData()
+  function getData () {
+    const URL = 'https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json'
 
-function showResult () {
-  const result = Quiz.userPoints
-  const total = Quiz.questions.length
-  actionButton.style.display = 'none'
-  questionsField.style.display = 'none'
-  quizTimer.style.display = 'none'
-  document.querySelector('.quiz-results').innerHTML = `<div class="sg-flash">
+    function processData (data) {
+      Quiz.timer = data.time_seconds
+      Quiz.questions = data.questions
+      console.log(Quiz)
+    }
+
+    return fetch(URL)
+      .then(response => response.json())
+      .then(data => processData(data))
+      .catch(error => console.log('Something went wrong', error))
+  }
+  getData()
+
+  function showResult () {
+    const result = Quiz.userPoints
+    const total = Quiz.questions.length
+    questionsField.style.display = 'none'
+    quizTimer.style.display = 'none'
+    actionButton.innerHTML = 'Try Again'
+    actionButton.onclick = () => resetQuiz()
+    quizResults.innerHTML = `<div class="sg-flash">
                                                          <div class="sg-flash__message">
                                                            <div class="sg-text sg-text--emphasised sg-text--standout sg-text--light">
                                                              Correct answers: <div class="sg-badge sg-badge--large sg-badge--blue-secondary-light">
@@ -46,60 +49,70 @@ function showResult () {
                                                            </div>
                                                          </div>
                                                        </div>`
-}
-
-function processAnswer () {
-  if (Quiz.currentQuestion < Quiz.questions.length) {
-    const answer = parseInt(document.querySelector('input[name=answer]:checked').getAttribute('id'))
-    if (answer === Quiz.correctAnswer) {
-      Quiz.userPoints += 1
-      console.log(Quiz.userPoints)
-    }
-    updateQuestion(Quiz.questions, Quiz.currentQuestion)
-  } else showResult()
-}
-
-function startQuiz () {
-  actionButton.onclick = () => processAnswer()
-  timer(Quiz.timer)
-  actionButton.innerHTML = 'Next'
-  actionButton.classList.add('sg-button-primary--disabled')
-  actionButton.setAttribute('disabled', '')
-  updateQuestion(Quiz.questions, Quiz.currentQuestion)
-}
-
-function updateQuestion (questions, index) {
-  const question = questions[index]
-  questionsField.innerHTML = loadQuestion(question)
-  Quiz.currentQuestion += 1
-  actionButton.classList.add('sg-button-primary--disabled')
-  actionButton.setAttribute('disabled', '')
-  const answerInputs = document.getElementsByClassName('quiz-questions__answer')
-  for (let i = 0; i < answerInputs.length; i++) {
-    answerInputs[i].addEventListener('click', function () {
-      actionButton.classList.remove('sg-button-primary--disabled')
-      actionButton.removeAttribute('disabled')
-    })
+    quizResults.style.display = 'block'
   }
-}
 
-function loadQuestion (question) {
-  return `<h3 class="quiz-questions__title">${question.question}</h3>
+  function processAnswer () {
+    if (Quiz.currentQuestion < Quiz.questions.length) {
+      const answer = parseInt(document.querySelector('input[name=answer]:checked').getAttribute('id'))
+      if (answer === Quiz.correctAnswer) {
+        Quiz.userPoints += 1
+      }
+      updateQuestion(Quiz.questions, Quiz.currentQuestion)
+    } else showResult()
+  }
+
+  function startQuiz () {
+    actionButton.onclick = () => processAnswer()
+    timer(Quiz.timer)
+    actionButton.innerHTML = 'Next'
+    actionButton.classList.add('sg-button-primary--disabled')
+    actionButton.setAttribute('disabled', '')
+    updateQuestion(Quiz.questions, Quiz.currentQuestion)
+  }
+
+  function resetQuiz () {
+    Quiz.userPoints = 0
+    Quiz.currentQuestion = 0
+    Quiz.correctAnswer = 0
+    questionsField.style.display = 'block'
+    quizTimer.style.display = 'block'
+    quizResults.style.display = 'none'
+    startQuiz()
+  }
+
+  function updateQuestion (questions, index) {
+    const question = questions[index]
+    questionsField.innerHTML = loadQuestion(question)
+    Quiz.currentQuestion += 1
+    actionButton.classList.add('sg-button-primary--disabled')
+    actionButton.setAttribute('disabled', '')
+    const answerInputs = document.getElementsByClassName('quiz-questions__answer')
+    for (let i = 0; i < answerInputs.length; i++) {
+      answerInputs[i].addEventListener('click', function () {
+        actionButton.classList.remove('sg-button-primary--disabled')
+        actionButton.removeAttribute('disabled')
+      })
+    }
+  }
+
+  function loadQuestion (question) {
+    return `<h3 class="quiz-questions__title">${question.question}</h3>
             <form class="quiz-questions__form">
                 ${loadAnswers(question.answers)}    
             </form>`
-}
+  }
 
-function updateCorrectAnswer (id) {
-  Quiz.correctAnswer = id
-}
+  function updateCorrectAnswer (id) {
+    Quiz.correctAnswer = id
+  }
 
-function loadAnswers (answers) {
-  return answers.map((answer) => {
-    if (answer.correct) {
-      updateCorrectAnswer(answer.id)
-    }
-    return `<div class="sg-label sg-label--secondary">
+  function loadAnswers (answers) {
+    return answers.map((answer) => {
+      if (answer.correct) {
+        updateCorrectAnswer(answer.id)
+      }
+      return `<div class="sg-label sg-label--secondary">
               <div class="sg-label__icon">
                   <div class="sg-radio">
                       <input class="sg-radio__element quiz-questions__answer" type="radio" id="${answer.id}" name="answer">
@@ -108,28 +121,29 @@ function loadAnswers (answers) {
               </div>
               <label class="sg-label__text" for="${answer.id}">${answer.answer}</label>
             </div>`
-  })
-}
+    })
+  }
 
-function timer (seconds) {
-  const presentTime = Date.now()
-  const finishTime = presentTime + seconds * 1000
-  displayTime(seconds)
+  function timer (seconds) {
+    clearInterval(countdown)
+    const presentTime = Date.now()
+    const finishTime = presentTime + seconds * 1000
+    displayTime(seconds)
 
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((finishTime - Date.now()) / 1000)
-    if (secondsLeft < 0) {
-      clearInterval(countdown)
-      showResult()
-    }
-    displayTime(secondsLeft)
-  }, 1000)
-}
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((finishTime - Date.now()) / 1000)
+      if (secondsLeft < 0) {
+        clearInterval(countdown)
+        showResult()
+      }
+      displayTime(secondsLeft)
+    }, 1000)
+  }
 
-function displayTime (seconds) {
-  const minutesLeft = Math.floor(seconds / 60)
-  const secondsLeft = seconds % 60
-  quizTimer.innerHTML = `<div class="sg-badge sg-badge--rounded">
+  function displayTime (seconds) {
+    const minutesLeft = Math.floor(seconds / 60)
+    const secondsLeft = seconds % 60
+    quizTimer.innerHTML = `<div class="sg-badge sg-badge--rounded">
                                <div class="sg-text sg-text--emphasised quiz-timer__time">
                                   ${minutesLeft}
                                   :
@@ -137,4 +151,7 @@ function displayTime (seconds) {
                                   ${secondsLeft}
                                </div>
                            </div>`
+  }
 }
+
+app()
